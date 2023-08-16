22c8475783dc3b390652468b200acb607593b5f1
w3villa-abhishek-anand <118361953+w3villa-abhishek-anand@users.noreply.github.com>
2023-08-17 00:18:13 +0530

    add dateOfJoining and profile in employee management.

--- models/employee.model.js
+++ models/employee.model.js
@@ -5,7 +5,9 @@ const employeeSchema = new Schema({
     name: {type:String, required:true},
     email: {type:String, required:true},
     password: {type:String, required:true},
-    isActive: {type:Boolean, default:true}
+    isActive: {type:Boolean, default:true},
+    dateOfJoining: {type: String, required:true},
+    profile: {type:String, required:true},
 });
 
 module.exports = mongoose.model('Employee', employeeSchema);
